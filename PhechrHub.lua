2f0bb59c5186e534ae4bde0ed750c9896d625498
mjkotan <107790204+mjkotan@users.noreply.github.com>
2023-11-13 10:04:39 +0700

    Update PhechrHub.lua

--- PhechrHub.lua
+++ PhechrHub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
+    Title = "PhechrHub " .. Fluent.Version,
+    SubTitle = "by Phechr",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
@@ -35,7 +35,13 @@ do
         Content = "This is a paragraph.\nSecond line!"
     })
 
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
 
+    Toggle:OnChanged(function()
+        print("Toggle changed:", Options.MyToggle.Value)
+    end)
+
+    Options.MyToggle:SetValue(false)
 
     Tabs.Main:AddButton({
         Title = "Button",
@@ -61,18 +67,6 @@ do
             })
         end
     })
-
-
-
-    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
-
-    Toggle:OnChanged(function()
-        print("Toggle changed:", Options.MyToggle.Value)
-    end)
-
-    Options.MyToggle:SetValue(false)
-
-
     
     local Slider = Tabs.Main:AddSlider("Slider", {
         Title = "Slider",

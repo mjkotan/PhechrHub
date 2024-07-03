d679163755268e108dc46162412bc26e9d860146
mjkotan <107790204+mjkotan@users.noreply.github.com>
2024-07-03 17:09:04 +0700

    Update AutoFarm-Level.lua

--- AutoFarm-Level.lua
+++ AutoFarm-Level.lua
@@ -1,8 +1,13 @@
-function checkLevel()
+function checkLevelAndPrint()
     local player = game:GetService("Players").LocalPlayer
     if player then
-        return player.Data.Level
+        local levelValue = player.Data.Level.Value
+        print("Current level value:", levelValue)
+        return levelValue
     else
         return nil  -- หรือค่าที่ต้องการในกรณีที่ไม่พบผู้เล่น
     end
 end
+
+-- เรียกใช้งานฟังก์ชัน checkLevelAndPrint()
+checkLevelAndPrint()
